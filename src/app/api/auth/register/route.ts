2e5402caba7dc4e323fddfeef0bd740e86d71701
leobbrd <leobbrd@gmail.com>
2025-05-28 04:08:55 +0900

    modify auth regisiter

--- src/app/api/auth/register/route.ts
+++ src/app/api/auth/register/route.ts
@@ -31,7 +31,7 @@ export async function POST(req: Request) {
       data: {
         name,
         email,
-        password: hashedPassword,
+        passwordHash: hashedPassword,
       },
     })
 
